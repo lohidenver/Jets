e8b1e808a9b3cf0bd6e3d3026becf0f02dd99eec
lohidenver <broadcastdave@gmail.com>
2021-08-06 16:31:49 -0600

    FC

---
+++ src/com/skilldistillary/jets/app/JetsApplication.java
@@ -0,0 +1,40 @@
+package com.skilldistillary.jets.app;
+
+import com.skilldistillery.jets.entities.AirField;
+
+public class JetsApplication {
+	private AirField airField;
+	
+	public static void main(String[] args) {
+
+		System.out.println("                                 /\\\n"
+				+ "1. List fleet                   |  |            J\n"
+				+ "2. Fly all jets                 |  |            E\n"
+				+ "3. View fastest jet            .'  '.           T\n"
+				+ "4. View jet with longest range |    |           S\n"
+				+ "5. Load all Cargo Jets         |    |\n"
+				+ "6. Dogfight!                   | /\\ |           A\n"
+				+ "7. Add a jet to Fleet        .' |__|'.          P\n"
+				+ "8. Remove a jet from Fleet   |  |  |  |         P\n"
+				+ "9. Quit                     .'  |  |  '.\n"
+				+ "                       /\\   |   \\__/   |   /\\\n"
+				+ "                      |  |  |   |  |   |  |  |\n"
+				+ "                  /|  |  |,-\\   |  |   /-,|  |  |\\\n"
+				+ "                  ||  |,-'   |  |  |  |   '-,|  ||\n"
+				+ "                  ||-'       |  |  |  |       '-||\n"
+				+ "       |\\     _,-'           |  |  |  |           '-,_     /|\n"
+				+ "       ||  ,-'   _           |  |  |  |           _   '-,  ||\n"
+				+ "       ||-'   SD30           |  |  |  |           SD30   '-||\n"
+				+ "       ||                    |  \\  /  |                    ||\n"
+				+ "       |\\________....--------\\   ||   /--------....________/|\n"
+				+ "                             /|  ||  |\\\n"
+				+ "                            / |  ||  | \\\n"
+				+ "                           /  |  \\/  |  \\\n"
+				+ "                          /   |      |   \\   \n"
+				+ "                        //   .|      |.   \\\\\n"
+				+ "                      .' |_./ |      | \\._| '.\n"
+				+ "                     /     _.-|||  |||-._     \\\n"
+				+ "                     \\__.-'   \\||/\\||/   '-.__/ ");
+	}
+
+}
